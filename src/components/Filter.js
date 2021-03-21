16aef9bab2175fa66098f30b6da49a5cfd176bb8
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-03-21 13:36:23 +0200

    clean

--- src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Filter = ({ value, onChangea }) => (
-  <label>
-    Filter <input type="text" value={value} onChange={onChangea} />
-  </label>
-);
-
-export default Filter;
